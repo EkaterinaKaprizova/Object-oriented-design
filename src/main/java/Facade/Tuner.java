6d7074e9cee4201bb0e6b4dfeb7a42bfbc366a95
Katya <kaprizovakatya@gmail.com>
2020-11-30 17:58:10 +0600

    Facade

---
+++ src/main/java/Facade/Tuner.java
@@ -0,0 +1,37 @@
+package Facade;
+
+public class Tuner  { //настройщик
+        private String description;
+        Amplifier amplifier;
+        private double frequency;
+
+        public Tuner(String description, Amplifier amplifier) {
+            this.description = description;
+        }
+
+        public void on() {
+            System.out.println(description + " on");
+        }
+
+        public void off() {
+            System.out.println(description + " off");
+        }
+
+        public void setFrequency(double frequency) {
+            System.out.println(description + " setting frequency to " + frequency);
+            this.frequency = frequency;
+        }
+
+        public void setAm() {
+            System.out.println(description + " setting AM mode");
+        }
+
+        public void setFm() {
+            System.out.println(description + " setting FM mode");
+        }
+
+        public String toString() {
+            return description;
+        }
+    }
+
