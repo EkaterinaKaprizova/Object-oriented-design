075fa8d5ad1d3a96b599082a76137362ebee1f93
Katya <kaprizovakatya@gmail.com>
2020-11-30 17:58:34 +0600

    Flyweight

---
+++ src/main/java/Flyweifght/ConiferTree.java
@@ -0,0 +1,7 @@
+package Flyweifght;
+
+public class ConiferTree implements Tree {
+    public void display(int x, int y) {
+        System.out.println("Conifer tree is located at " + x + ", " + y);
+    }
+}
---
+++ src/main/java/Flyweifght/Tree.java
@@ -0,0 +1,13 @@
+package Flyweifght;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public interface Tree {
+    void display(int x, int y);
+
+    default boolean isWithinRange(LocalDate aDate) {
+        Month month = aDate.getMonth();
+        return (month.getValue() > 2) && (month.getValue() < 11);
+    }
+}
