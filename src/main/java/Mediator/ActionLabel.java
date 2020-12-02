eb6a3941023357ec42738eaa2aea7af148b5c9aa
Katya <kaprizovakatya@gmail.com>
2020-12-02 11:39:14 +0600

    Посредник

---
+++ src/main/java/Mediator/ActionLabel.java
@@ -0,0 +1,12 @@
+package Mediator;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ActionLabel extends JLabel {
+
+    public ActionLabel(String text) {
+        super(text);
+        setPreferredSize(new Dimension(130, 25));
+    }
+}
---
+++ src/main/java/Mediator/Main.java
@@ -0,0 +1,8 @@
+package Mediator;
+
+public class Main {
+    public static void main(String[] args) {
+        Window w = new Window();
+        w.setVisible(true);
+    }
+}
